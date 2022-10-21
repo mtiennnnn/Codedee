1f2c44f498ca6be00c2cc118ef0db55435b15d81
mtiennnnn <20522008@gm.uit.edu.vn>
2022-10-21 20:26:46 +0700

    modified some files

--- home/static/js/materialize.js
+++ home/static/js/materialize.js
@@ -12372,3 +12372,10 @@ $jscomp.polyfill = function (e, r, p, m) {
 
   Range.init($('input[type=range]'));
 })(cash, M.anime);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {accordion: true});
+}); 
+
