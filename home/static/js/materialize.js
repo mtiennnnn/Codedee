2f675dd2097ade5b57f561cfc8efb7a374ccb741
meobeongok <20521859@gm.uit.edu.vn>
2022-10-22 00:14:14 -0700

    tyle nguyen lam rules

--- home/static/js/materialize.js
+++ home/static/js/materialize.js
@@ -12383,3 +12383,21 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, {alignment: "left"});
   });
+  
+//rules js
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
